633955dc7262984cbfc245deaecae4b8675d3fb9
anhaki <m.anugrahhakiki@gmail.com>
2024-12-12 12:38:09 +0700

    adm template

---
+++ pmb_haki/resources/views/layouts/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" class="w-full h-full bg-gray-200">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @yield('style')
+</head>
+
+<body>
+    @include('components.sidebar')
+
+    <div class="md:pl-64 pt-20 pl-4 pr-4 pb-4 text-sm">
+        <div class="bg-white p-4 rounded-lg divide-y-2 divide-solid shadow-md">
+            @yield('content')
+        </div>
+    </div>
+    @yield('script')
+</body>
+
+</html>
--- pmb_haki/resources/views/welcome.blade.php
+++ pmb_haki/resources/views/welcome.blade.php
@@ -4,11 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PMB UMDP</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body class="">
